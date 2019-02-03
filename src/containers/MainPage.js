5c140b2a1f40ba6ae71f31bde07b3a1cf2a4419a
Jedsada Tiwongvorakul <jt@20scoops.net>
2019-02-03 16:11:22 +0700

    remove import unused

--- src/containers/MainPage.js
+++ src/containers/MainPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Layout, Menu, Modal, Button, Divider } from 'antd';
+import { Layout, Menu, Modal, Button } from 'antd';
 import RouteMenu from './RouteMenu';
 import { connect } from 'react-redux';
 
